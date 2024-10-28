10c0ac66784c4fd6a31b250415164ebee1d50789
Ong Tangjintanakarn <144099839+ong-palo@users.noreply.github.com>
2024-10-28 08:39:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+  return {
+    skills: ['JavaScript', 'Node.js', 'React', 'CSS', 'HTML', 'Python', 'Django', 'Java', 'Spring', 'Kotlin', 'Swift', 'iOS', 'Android', 'Flutter'],
+  };
+}
